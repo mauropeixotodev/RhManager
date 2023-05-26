5f1770c90760f5fe348a15f505ace5cef00a05f8
unknown <mauroaugustodev@gmail.com>
2023-05-26 08:52:37 -0300

    ajustes end point cargo

--- www/src/main/java/com/rhmanager/www/controller/cargo/CargoController.java
+++ www/src/main/java/com/rhmanager/www/controller/cargo/CargoController.java
@@ -1,5 +1,72 @@
 package com.rhmanager.www.controller.cargo;
 
-public class CargoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.rhmanager.www.controller.cargo.request.CargoRequest;
+import com.rhmanager.www.controller.cargo.response.CargoResponse;
+import com.rhmanager.www.service.CargoService;
+
+public class CargoController {
+	@Autowired
+	CargoService cargoService;
+
+	@PostMapping
+	public ResponseEntity<CargoResponse> cadastrar(@RequestBody @Validated CargoRequest cargoRequest)
+			throws Exception {
+		try {
+			return ResponseEntity.ok(cargoService.salvar(cargoRequest));
+		} catch (Exception e) {
+			throw new Exception("Erro ao criar o cargo: " + e);
+		}
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<CargoResponse> buscar(@PathVariable Long id) throws Exception {
+		try {
+			return ResponseEntity.ok(cargoService.buscar(id));
+		} catch (Exception e) {
+			throw new Exception("Erro ao buscar cargo: " + e);
+		}
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CargoResponse>> listar() throws Exception {
+		try {
+			return ResponseEntity.ok(cargoService.listar());
+		} catch (Exception e) {
+			throw new Exception("Erro ao listar cargos: " + e);
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<CargoResponse> modificar(@PathVariable Long id,
+			@RequestBody @Validated CargoRequest cargoRequest) throws Exception {
+		try {
+			return ResponseEntity.ok(cargoService.atualizar(id, cargoRequest));
+		} catch (Exception e) {
+			throw new Exception("Erro ao atualizar cargo: " + e);
+		}
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletar(@PathVariable Long id) throws Exception {
+		try {
+			cargoService.deletar(id);
+			return ResponseEntity.ok().build();
+		} catch (Exception e) {
+			throw new Exception("Erro ao deletar cargo: " + e);
+		}
+
+	}
 }
--- www/src/main/java/com/rhmanager/www/service/CargoService.java
+++ www/src/main/java/com/rhmanager/www/service/CargoService.java
@@ -1,6 +1,9 @@
 package com.rhmanager.www.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.rhmanager.www.controller.cargo.request.CargoRequest;
 import com.rhmanager.www.controller.cargo.response.CargoResponse;
 import com.rhmanager.www.conversor.CargoConversor;
+import com.rhmanager.www.model.Cargo;
 import com.rhmanager.www.repository.CargoRepository;
 
 @Service
@@ -32,6 +36,32 @@ public class CargoService {
 
 	public List<CargoResponse> listar() {
 		return cargoConversor.CargoBuildResponse(cargoRepository.findAll());
+	}
+	@Transactional
+	public CargoResponse atualizar(Long id, CargoRequest cargoRequest) {
+		Optional<Cargo> CargoOpitional = cargoRepository.findById(id);
+		if (CargoOpitional.isPresent()) {
+			Cargo cargo = CargoOpitional.get();
+			if (cargoRequest.getNome() != null) {
+				cargo.setNome(cargoRequest.getNome());
+			}
+			if (cargoRequest.getDescricao() != null) {
+				cargo.setDescricao(cargoRequest.getDescricao());
+			}
+			if (cargoRequest.getSalario() != null) {
+				cargo.setSalario(cargoRequest.getSalario());;
+			}
+			return cargoConversor.CargoBuildResponse(cargo);
+		}
+		throw new Error("Funcionário não encontrado");
+
+	}
+	public void deletar(Long id) {
+		try {
+			cargoRepository.deleteById(id);
+		} catch (Exception e) {
+			throw new Error("Erro ao deletar usuário: " + e);
+		}
 	}
 
 }
